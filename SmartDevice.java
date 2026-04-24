a2aa3b5c056e0508ceebef0a4d779cc958bee5ee
jermzkk <veremiarealmi@gmail.com>
2026-04-24 21:21:35 +0700

    Create SmartDevice.java

---
+++ SmartDevice.java
@@ -0,0 +1,9 @@
+public abstract class SmartDevice {
+    protected String ipAddress;
+
+    public SmartDevice(String ipAddress) {
+        this.ipAddress = ipAddress;
+    }
+
+    public abstract void turnOn();
+}
